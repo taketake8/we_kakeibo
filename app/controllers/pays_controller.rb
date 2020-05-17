eea47e8233e3d560d530f7033ef839bf86debf48
taketake8 <62596306+taketake8@users.noreply.github.com>
2020-05-18 00:40:23 +0900

    pay.indexサーバーサイド

--- app/controllers/pays_controller.rb
+++ app/controllers/pays_controller.rb
@@ -4,7 +4,8 @@ class PaysController < ApplicationController
   # GET /pays
   # GET /pays.json
   def index
-    @pays = Pay.all
+    @pays = Pay.includes(:user)
+    @user = User.find_by(id: current_user.id)
   end
 
   # GET /pays/1
@@ -69,6 +70,6 @@ class PaysController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def pay_params
-      params.require(:pay).permit(:start_time：datetime)
+      params.require(:pay).permit(:start_time,:price, :memo).merge(user_id: current_user.id)
     end
 end
