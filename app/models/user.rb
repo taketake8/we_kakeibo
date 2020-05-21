278e50874a1b3c5e81b810de52777ba9b65af2e2
taketake8 <62596306+taketake8@users.noreply.github.com>
2020-05-21 11:03:51 +0900

    user.rbバリテーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :pays
+  validates :nickname, :age, presence:true
+  validates :nickname,  uniqueness: true
+  validates :nickname, length: { maximum: 8 }
+
 end
